ebf57e1f8abee00052fbfb8a1c39bb28d95e1f86
Erjankydyrov <erjankydyrov29@gmail.com>
2021-02-19 13:55:39 +0600

    added let td1

--- scripts.js
+++ scripts.js
@@ -24,6 +24,7 @@ fetch('./data.json')
     .then(function(json){
     for (const key in json) {
         let tr = document.createElement('tr');
+        let td1 = document.createElement('td');
         
     }
 });    
