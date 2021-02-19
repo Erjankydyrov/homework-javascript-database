0d1994ebff5aea232646f15d8fcc0217390d0e42
Erjankydyrov <erjankydyrov29@gmail.com>
2021-02-19 13:52:00 +0600

    added table

---
+++ scripts.js
@@ -0,0 +1,13 @@
+let table = document.createElement('table');
+let tr = document.createElement('tr');
+let th1 = document.createElement('th');
+let th2 = document.createElement('th');
+let th3 = document.createElement('th');
+let th4 = document.createElement('th');
+
+th1.innerText = "Number";
+th2.innerText = "Company";
+th3.innerText = "Games";
+th4.innerText = "Pop-games";
+
+table.append(tr);
